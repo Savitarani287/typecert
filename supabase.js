0c3ca795395a57461910936404e097dd20cddd3c
Savitarani287 <Savitaarani952@gamil.com>
2026-06-23 17:06:28 +0530

    Add Supabase auth

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 
 
-const SUPABASE_URL  = 'https://oakuzklpkqoyxvsdaaji.supabase.co/rest/v1/';      // e.g. https://xyzabc.supabase.co
+const SUPABASE_URL  = 'https://oakuzklpkqoyxvsdaaji.supabase.co';      // e.g. https://xyzabc.supabase.co
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ha3V6a2xwa3FveXh2c2RhYWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDYwNjcsImV4cCI6MjA5Nzc4MjA2N30.oI_77fmm0NO7x74qBmbrPXKNLSf0nl7j0c5QfG4HtZ0';  // starts with eyJ...
 
 // ─── DO NOT EDIT BELOW THIS LINE ─────────────────────────────────────────────
